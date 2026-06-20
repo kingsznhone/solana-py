d57e3b8feb428997c98f7781b2fe7b485e52cf0a
kingsznhone <kingsznhone@gmail.com>
2026-06-20 15:58:11 +0800

    ruff format

--- src/solana/rpc/providers/base.py
+++ src/solana/rpc/providers/base.py
@@ -10,8 +10,6 @@ T = TypeVar("T")
 class BaseProvider:
     """Base class for RPC providers to implement."""
 
-    def make_request(
-        self, body: JsonRPCRequestSerializer, parser: JsonRPCResponseParserType[T]
-    ) -> T:
+    def make_request(self, body: JsonRPCRequestSerializer, parser: JsonRPCResponseParserType[T]) -> T:
         """Make a request to the rpc endpoint."""
         raise NotImplementedError("Providers must implement this method")
